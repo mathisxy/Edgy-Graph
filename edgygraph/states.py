b44476b411b0ad94d8ebef02009c9d8961d19bbc
mathisxy <mathis.siebert@gmail.com>
2026-01-15 13:17:37 +0000

    added Streaming State

--- edgygraph/states.py
+++ edgygraph/states.py
@@ -1,4 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import TypeVar, AsyncIterator, Protocol, Generic
+from types import TracebackType
 
 class GraphState(BaseModel):
-    vars: dict[str, object] = {}
+    vars: dict[str, object] = Field(default_factory=dict)
+
+
+T = TypeVar('T', covariant=True)
+
+class Stream(AsyncIterator[T], Protocol):
+    async def aclose(self) -> None: ...
+
+    async def __aenter__(self) -> "Stream[T]":
+        return self
+
+    async def __aexit__(
+            self, exc_type: type[BaseException] | None, 
+            exc: BaseException | None, 
+            tb: TracebackType | None
+        ) -> None: # Not handling exceptions here -> returns None
+
+        await self.aclose()
+
+
+class GraphStateStream(GraphState, Generic[T]):
+    
+    stream: Stream[T] | None = None
