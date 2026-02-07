c08bdfc139575b789448f30a7a70f76ba2f9d856
mathisxy <mathis.siebert@gmail.com>
2026-02-07 15:52:19 +0000

    .

--- src/edgygraph/nodes.py
+++ src/edgygraph/nodes.py
@@ -1,9 +1,8 @@
-from .states import StateProtocol, SharedProtocol
-from typing import Protocol, runtime_checkable
+from .states import State, Shared
 from abc import ABC, abstractmethod
 
 
-class Node(ABC):
+class Node[T: State, S: Shared](ABC):
     """
     Represents a node in the graph.
 
@@ -11,12 +10,10 @@ class Node(ABC):
     Due to variance the node can also take any subtype of the state and shared state.
 
     The node must implement the `run` method, which will be called when the node is executed.
-    The argument types of the `run` method define the type of state and shared state that the node expects.
-    Due to variance the node can also take any subtype of the state and shared state.
     """
     
     @abstractmethod
-    async def run(self, state: StateProtocol, shared: SharedProtocol) -> None:
+    async def run(self, state: T, shared: S) -> None:
         """
         Runs the node with the given state and shared state from the graph.
 
@@ -27,25 +24,6 @@ class Node(ABC):
         Therefore, the shared state is shared between nodes and operations are not safe without using the Lock.
         The lock can be accessed via `shared.lock`.
 
-        Args:
-            state: The state of the graph.
-            shared: The shared state of the graph.
-
-        Returns:
-            None. The instance references of the arguments are used in the graph to enable variance.
-        """
-        pass
-
-
-
-@runtime_checkable
-class NodeProtocol[T: StateProtocol, S: SharedProtocol](Protocol):
-    """
-    Protocol for nodes in the graph. This is used for type checking and to allow for more flexible node definitions.
-    """
-    
-    async def run(self, state: T, shared: S) -> None:
-        """
         Args:
             state: The state of the graph.
             shared: The shared state of the graph.
