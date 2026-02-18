1442a223bf5d4ac529223c685ddabf4aacfca35a
mathisxy <mathis.siebert@gmail.com>
2026-02-18 03:09:18 +0000

    fixed docstrings

--- src/edgygraph/graph_hooks.py
+++ src/edgygraph/graph_hooks.py
@@ -20,77 +20,97 @@ class GraphHook[T: State, S: Shared](ABC):
     They can be used to log, modify the state, or perform other actions.
     """
 
-    async def on_graph_start(self, state: T, shared: S): pass
-    """
-    Called when the graph starts.
+    async def on_graph_start(self, state: T, shared: S) -> None:
+        """
+        Called when the graph starts.
 
-    Args:
-        state: The initial state of the graph.
-        shared: The initial shared state of the graph.
-    """
+        Args:
+            state: The initial state of the graph.
+            shared: The initial shared state of the graph.
+        """
 
-    async def on_step_start(self, state: T, shared: S, nodes: list[Node[T, S]]): pass
-    """
-    Called when a step starts.
+        pass
 
-    Args:
-        state: The state of the graph.
-        shared: The shared state of the graph.
-        nodes: The nodes that will be executed in this step.
-    """
 
-    async def on_step_end(self, state: T, shared: S, nodes: list[Node[T, S]]): pass
-    """
-    Called when a step ends.
+    async def on_step_start(self, state: T, shared: S, nodes: list[Node[T, S]]) -> None:
+        """
+        Called when a step starts.
 
-    It is called after all nodes have been executed and the state has been merged.
+        Args:
+            state: The state of the graph.
+            shared: The shared state of the graph.
+            nodes: The nodes that will be executed in this step.
+        """
 
-    Args:
-        state: The updated state of the graph.
-        shared: The shared state of the graph.
-        nodes: The nodes that were executed in this step.
-    """
+        pass
 
-    async def on_merge_start(self, state: T, result_states: list[T], changes: list[dict[str, "Change"]]): pass
-    """
-    Called when the merge process starts.
+
+    async def on_step_end(self, state: T, shared: S, nodes: list[Node[T, S]]) -> None:
+        """
+        Called when a step ends.
+
+        It is called after all nodes have been executed and the state has been merged.
+
+        Args:
+            state: The updated state of the graph.
+            shared: The shared state of the graph.
+            nodes: The nodes that were executed in this step.
+        """
+
+        pass
+
+
+    async def on_merge_start(self, state: T, result_states: list[T], changes: list[dict[str, "Change"]]) -> None:
+        """
+        Called when the merge process starts.
+        
+        Args:
+            state: The old state of the graph.
+            result_states: The result states of the nodes.
+            changes: The changes that will be applied to the state.
+        """
+
+        pass
+
+
+    async def on_merge_conflict(self, state: T, result_states: list[T], changes: list[dict[str, Change]], conflicts: dict[str, list["Change"]]) -> None:
+        """
+        Called when a merge conflict occurs.
+        
+        Args:
+            state: The old state of the graph.
+            result_states: The result states of the nodes.
+            changes: The changes that will be applied to the state.
+            conflicts: The conflicts that occurred during the merge process.
+        """
+
+        pass
+
+
+    async def on_merge_end(self, state: T, result_states: list[T], changes: list[dict[str, "Change"]], merged_state: T) -> None:
+        """
+        Called when the merge process ends.
+        
+        Args:
+            state: The old state of the graph.
+            result_states: The result states of the nodes.
+            changes: The changes that have been applied to the state.
+            merged_state: The new merged state of the graph.
+        """
+
+        pass
     
-    Args:
-        state: The old state of the graph.
-        result_states: The result states of the nodes.
-        changes: The changes that will be applied to the state.
-    """
 
-    async def on_merge_conflict(self, state: T, result_states: list[T], changes: list[dict[str, Change]], conflicts: dict[str, list["Change"]]): pass
-    """
-    Called when a merge conflict occurs.
-    
-    Args:
-        state: The old state of the graph.
-        result_states: The result states of the nodes.
-        changes: The changes that will be applied to the state.
-        conflicts: The conflicts that occurred during the merge process.
-    """
+    async def on_graph_end(self, state: T, shared: S) -> None:
+        """
+        Called when the graph execution ends.
 
-    async def on_merge_end(self, state: T, result_states: list[T], changes: list[dict[str, "Change"]], merged_state: T): pass
-    """
-    Called when the merge process ends.
-    
-    Args:
-        state: The old state of the graph.
-        result_states: The result states of the nodes.
-        changes: The changes that have been applied to the state.
-        merged_state: The new merged state of the graph.
-    """
-    
-    async def on_graph_end(self, state: T, shared: S): pass
-    """
-    Called when the graph execution ends.
+        Args:
+            state: The final state of the graph.
+            shared: The final shared data.
+        """
 
-    Args:
-        state: The final state of the graph.
-        shared: The final shared data.
-    """
+        pass
 
 
 
